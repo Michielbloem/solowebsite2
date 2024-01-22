adda1d6c8828601c24ead0bc7e5cb91868504433
Michielbloem <157414546+Michielbloem@users.noreply.github.com>
2024-01-22 20:38:51 +0100

    Add files via upload

---
+++ global.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Deze functie wordt uitgevoerd zodra de pagina geladen is
+
+    const timeline = document.getElementById('timeline');
+    // Je kunt hier logica toevoegen om de tijdlijn dynamisch te vullen
+});
